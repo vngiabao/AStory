35fceaedfeeec2d5534acedb9b469219fbfead3e
T140805 <102812129+teresakatu@users.noreply.github.com>
2026-06-12 22:04:52 +0700

    Fixed host and port for render

--- AI/app.py
+++ AI/app.py
@@ -149,5 +149,8 @@ def transcribe():
     return jsonify({"transcript": result.text})
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
